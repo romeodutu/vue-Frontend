59321634f8d9cdea14fe8036429238f6f332a972
Romeo <romeo_dutu@yahoo.com>
2019-03-05 02:44:35 +0200

    Update About.hero.vue
    SEO improvement

--- src/client/components/heros/About.hero.vue
+++ src/client/components/heros/About.hero.vue
@@ -8,17 +8,17 @@
                 <span class="pageTitle alignCenter titleMarginBottom removeBackground aboutTitle">What is WebDollar?</span>
                 <div id="whatContainer">
                     <div>
-                        <img class="lazy" data-src="/public/assets/images/WebDollar-ming.png" alt="webdollar-mining">
+                        <img class="lazy" data-src="/public/assets/images/WebDollar-ming.png" alt="Browser Hybrid Mining" title="Browser Hybrid Mining">
                         <h4>Start Mining in seconds</h4>
                         <p>You can start mining WEBD immediately with the first blockchain running directly in your Browser, on any device</p>
                     </div>
                     <div>
-                        <img class="lazy" data-src="/public/assets/images/WebDollar-Security.png" alt="webdollar-security">
+                        <img class="lazy" data-src="/public/assets/images/WebDollar-Security.png" alt="Secure Blockchain" title="Secure Blockchain">
                         <h4>Secure and Private</h4>
                         <p>Built on the most advanced blockchain technology using NIPoPoW consensus, your information will be secure and anonymous</p>
                     </div>
                     <div>
-                        <img class="lazy" data-src="/public/assets/images/WebDollar-wallet.png" alt="webdollar-wallet">
+                        <img class="lazy" data-src="/public/assets/images/WebDollar-wallet.png" alt="Easy Integration" title="Easy Integration">
                         <h4>Simple Web Payments</h4>
                         <p>With easy browser integrated blockchain for peer-to-peer transactions, allowing cross-border blockchain payments</p>
                     </div>
