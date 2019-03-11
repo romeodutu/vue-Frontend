e4f0b89c58630ffeae5614577c97abe69b0cf8f5
Romeo <romeo_dutu@yahoo.com>
2019-03-11 21:23:17 +0200

    Create Exchange.hero.vue
    SEO Improvement
    Built Exchange page

---
+++ src/client/components/heros/Exchange.hero.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <div id="exchangeSection">
+
+        <span class="alignCenter bigMarginTop pageTitle">Exchange Cryptocurrency</span>
+
+        <div class="faqContainer sectionCenteredWidth">
+            <div class="faqSection">
+			Although the developers team worked at their own exchange in the first place to prepare the implementation code for the new exchanges and secondly to have a first exchange, then to develop the code in general, here is someone in the community has already listed the WebDollar cryptocurrency on its small exchange gymbase.io.
+            Soon it will be listed on Top 15 CryptoExchange from CMC - p2pb2b.io
+            Then listed on the biggest CryptoExchange in the world.
+			Open, secure, organized where buyers and sellers present the demand and offer and the price automatically balances. The price is also adjusted externally according to several criteria such as the utility given by cryptocurrency, mass adoption, simplicity, etc. The Exchange has a real-time transaction mechanism.
+            </div>
+        </div>
+    </div>
+</template>
