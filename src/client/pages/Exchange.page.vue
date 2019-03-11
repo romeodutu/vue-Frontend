c02c60808ddbb35975942c7a875116087d33c778
Romeo <romeo_dutu@yahoo.com>
2019-03-11 21:23:22 +0200

    Create Exchange.page.vue
    SEO Improvement
    Built Exchange page

---
+++ src/client/pages/Exchange.page.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <layout>
+
+        <div slot="content">
+
+            <exchange-hero/>
+
+        </div>
+
+    </layout>
+
+</template>
+
+<script>
+
+    import Layout from "client/components/layout/sampleLayout.vue"
+    import ExchangeHero from "client/components/heros/Exchange.hero.vue"
+
+
+    export default {
+
+        name: "exchange-page",
+
+        components:{
+            "layout": Layout,
+            "exchange-hero": ExchangeHero,
+
+        }
+
+    }
+
+</script>
