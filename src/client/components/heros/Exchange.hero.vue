9fffcc70f469d8beff8a0c94b3ac70f77ebc6079
Romeo <romeo_dutu@yahoo.com>
2019-03-21 13:16:31 +0200

    Update Exchange.hero.vue
    SEO improvements

--- src/client/components/heros/Exchange.hero.vue
+++ src/client/components/heros/Exchange.hero.vue
@@ -6,8 +6,8 @@
 
         <div class="faqContainer sectionCenteredWidth">
             <div class="faqSection">
-			Although the developers team worked at their own exchange in the first place to prepare the implementation code for the new exchanges and secondly to have a first exchange, then to develop the code in general, here is someone in the community has already listed the WebDollar cryptocurrency on its small exchange gymbase.io.
-            On 20 March 2019 WebDollar was listed on Top 15 CryptoExchange from CMC - p2pb2b.io
+			Although the developers team worked at their own exchange in the first place to prepare the implementation code for the new exchanges and secondly to have a first exchange, then to develop the code in general, here is someone in the community has already listed the WebDollar cryptocurrency on its small exchange <a href="https://gymbase.io" title="CryptoExchange Gymbase">gymbase.io</a>.
+            On 20 March 2019 WebDollar was listed on Top 15 CryptoExchange from CMC - <a href="https://p2pb2b.io" title="CryptoExchange P2pb2b">p2pb2b.io</a>.
             Soon will be listed on the biggest CryptoExchange in the world.
 			Open, secure, organized where buyers and sellers present the demand and offer and the price automatically balances. The price is also adjusted externally according to several criteria such as the utility given by cryptocurrency, mass adoption, simplicity, etc. The Exchange has a real-time transaction mechanism.
             </div>
