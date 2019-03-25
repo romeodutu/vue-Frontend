13ccb7c3f0b957b729c9f29704fc5badaf00433f
Romeo <romeo_dutu@yahoo.com>
2019-03-25 13:28:15 +0200

    Update Exchange.hero.vue
    Arange text and image for Exchange content.

--- src/client/components/heros/Exchange.hero.vue
+++ src/client/components/heros/Exchange.hero.vue
@@ -6,16 +6,18 @@
 
         <div class="faqContainer sectionCenteredWidth">
             <div class="collapse-content">
-			<p>GYMbase.io was the first exchange that saw the potential of our project and listed WebDollar without even being asked for it. 
+			<p>GYMbase.io was the first exchange that saw the potential of our project and listed WebDollar without even being asked for it.</p> 
 			<p>WebDollar started trading for its first time on GYMbase.io on February 8th of 2019, just a few days after hybrid (POW + POS) mining was implemented.</p>
 			<p>Because our community was eager for an exchange, GYMbase.io soon became overwhelmed by the impressive numbers of WebDollar's trades and volume.</p>
 			<p><a href="https://gymbase.io" title="CryptoExchange Gymbase">gymbase.io - START TRADE NOW</a></p>
+			<p><a href="https://gymbase.io" title="CryptoExchange Gymbase"><img alt="CryptoExchange Gymbase" title="CryptoExchange Gymbase" src="/public/assets/images/exchange/exchange-gymbase.png"  height="25" width="106"></a></p>
             <p><br></p>
 			<p>P2pb2b.io proved to be the perfect big step for WebDollar after hybrid (POW + POS) mining implementation. A top 20 by volume on cmc.io, p2pb2b.io is the fastest growing cryptocurrency exchange in the world.</p>
             <p>WebDollar was listed and traded for the first time on the 20th of March 2019. Only a few days after the listing, our Telegram reached over 10k community members while our Twitter jumped over the 5k followers mark.</p>
 			<p>A trading market volume of almost half a billion US dollars made p2pb2b extremely appealing to WebDollar miners, enthusiasts, early adopters, and new owners, for in only a few days after the listing, more than 70 million WEBD was transferred towards the exchange.</p> 
             <p>Pairings are also a true trader’s delight since WEBD is traded on p2pb2b against Ethereum, Bitcoin, and USD.</p>
 			<p><a href="https://p2pb2b.io" title="CryptoExchange P2pb2b">p2pb2b.io - START TRADE NOW</a></p>
+			<p><a href="https://p2pb2b.io" title="CryptoExchange P2pb2b"><img alt="CryptoExchange P2pb2b" title="CryptoExchange P2bp2b" src="/public/assets/images/exchange/exchange-p2pb2b.png"  height="25" width="106"></a></p>
             </div>
         </div>
     </div>
