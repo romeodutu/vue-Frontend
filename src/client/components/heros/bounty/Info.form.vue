88c74c5bf28756177e38a8ebdae23e85e561a6a1
Ionut Moraru <morion4000@gmail.com>
2019-05-25 18:23:26 +0300

    updates

--- src/client/components/heros/bounty/Info.form.vue
+++ src/client/components/heros/bounty/Info.form.vue
@@ -41,13 +41,11 @@
       <span class="infoLine">
         <b>Bounty round:</b>
         <div style="display: inline-block">
-          <select id="poolConnectionSelect" class="poolSelect" @change="handleConnectionSelect">
-            <option class="poolSelectOption" >Current Round</option>
-            <option class="poolSelectOption" >12.05.2019 - 19.05.2019</option>
-            <!--<option v-for="(poolListElement, index) in this.poolsList" class="poolSelectOption"  >-->
-            <!--{{poolListElement.poolName}}-->
-            <!--</option>-->
-            </select>
+          <select id="poolConnectionSelect" class="poolSelect" @change="handleChange($event)">
+            <option v-for="(elem, index) in this[this.type].bounties" class="poolSelectOption">
+              {{elem.name}}
+            </option>
+          </select>
         </div>
       </span>
       <span class="infoLine">
@@ -228,6 +226,21 @@ export default {
   },
 
   methods: {
+    handleChange(event) {
+      let index = 0;
+      let selected = event.target.value;
+      let bounties = this[this.type].bounties;
+
+      for (var i=0; i<bounties.length; i++) {
+        if (bounties[i].name === selected) {
+          index = i;
+          break;
+        }
+      }
+
+      this[this.type] = bounties[index];
+      this[this.type].bounties = bounties;
+    },
 
     countDown() {
 
@@ -280,6 +293,7 @@ export default {
       // Get last bounty
       let bounty = twitter_call.data[twitter_call.data.length - 1];
       this.twitter = bounty;
+      this.twitter.bounties = twitter_call.data;
       this.twitter.update = '';
     }
 
@@ -289,6 +303,7 @@ export default {
       // Get last bounty
       let bounty = youtube_call.data[youtube_call.data.length - 1];
       this.youtube = bounty;
+      this.youtube.bounties = youtube_call.data;
       this.youtube.update = '';
     }
 
